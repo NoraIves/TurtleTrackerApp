6d3786c10cc0b51786ae7dbf67213dcc5a2c4a5e
NoraIves <eni5@duke.edu>
2021-09-29 09:09:15 -0400

    Iterate through all records for real this time

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -9,18 +9,31 @@
 # Date:   Fall 2021
 #--------------------------------------------------------------
 
-#Pretend we read one line of data from the file
-lineString = '20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0'
+#Create a variable pointing to the data file
+file_name = './Data/Raw/sara.txt'
 
-#Split the string into a list of data items
-lineData = lineString.split()
+#Create a file object from the file
+file_object = open(file_name, 'r')
 
-#Extract items in list into variables 
-record_id = lineData[0]
-obs_date = lineData[2]
-obs_lc = lineData[4]
-obs_lat = lineData[6]
-obs_lon = lineData[7]
+#Read contents of file into a list
+line_list = file_object.readlines()
 
-#Print the location of sara
-print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat}, lon:{obs_lon} on {obs_date}")
+#Close the file
+file_object.close()
+
+#Iterate through all lines in the linelist
+for lineString in line_list:
+    if lineString[0] == "#" : continue 
+
+    #Split the string into a list of data items
+    lineData = lineString.split()
+    
+    #Extract items in list into variables 
+    record_id = lineData[0]
+    obs_date = lineData[2]
+    obs_lc = lineData[4]
+    obs_lat = lineData[6]
+    obs_lon = lineData[7]
+    
+    #Print the location of sara
+    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat}, lon:{obs_lon} on {obs_date}")
